2e5bf407f7af7ae95f1e9611f0f06ca2ededdd30
rater <rater193@gmail.com>
2025-11-05 08:08:36 -0500

    Updated GearForgeMod.java

--- src/main/java/com/rater193/gearforge/GearForgeMod.java
+++ src/main/java/com/rater193/gearforge/GearForgeMod.java
@@ -3,6 +3,7 @@ package com.rater193.gearforge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import com.rater193.gearforge.network.ModNetwork;
 
 @Mod(GearForgeMod.MODID)
 public class GearForgeMod {
@@ -15,5 +16,6 @@ public class GearForgeMod {
         ModMenuTypes.register(bus);
         com.rater193.gearforge.upgrade.UpgradeRegistry.init();
         EventHandlers.register();
+        ModNetwork.init();
     }
 }
